af86195c4944ef4e96da2a8ed29f08cff35e2022
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-24 18:03:07 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes, NavLink, } from 'react-router-dom';
+import Admin from './Components/Admin'
 
 function App() {
   
 
   return (
     <>
-      
+    <BrowserRouter>
+    <Routes>
+    <Route path='/Admin' element={<Admin />} />
 
-      
+    </Routes>
+    </BrowserRouter>  
     </>
   )
 }
