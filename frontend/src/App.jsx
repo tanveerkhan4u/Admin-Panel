84fd97850e8d46530b3e25973d8f0fd79ec4998b
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-26 12:23:01 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './Components/Sidebar'
+import Home from './Components/Home'
 
 
 
 const App = () => {
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid bg-secondary '>
       <div className='row'>
-        <div className='col-2'>
+        <div className='col-2 bg-white vh-100'>
           <Sidebar />
         </div>
         <div className='col'>
-
+<Home />
         </div>
       </div>
     </div>
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-white navbar-expand-lg bg-white px-3">
+        <div className="container-fluid">
+          <i className="fa-solid fa-bars bor fs-5 ">
+            
+          </i>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+             
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  John Doe
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                  
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Log Out
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              
+            </ul>
+           
+          </div>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
+
+export default Navbar;
